fa4210bc2df1f7899ea0d9e10bab650b1cc65383
usman1486 <usmanakhtar1486@gmail.com>
2022-07-03 14:45:56 -0700

    MY FIRST JAVA FILE

---
+++ House.java
@@ -0,0 +1,14 @@
+public class House {
+    String city;
+    int street;
+
+    public House(String c, int s) {
+        this.city = c;
+        this.street = s;
+    }
+
+    void disp1() {
+        System.out.println("ciyt is-->" + city + "street no-->" + street);
+    }
+
+}
